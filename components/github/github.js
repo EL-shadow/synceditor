b5b62bd89925fa499c3387d1e22a9e78c284a0d0
Elnur Kurtaliev <el92@yandex.ru>
2020-06-21 00:23:28 +0300

    Методы для отправки изменений в коммиты на гитхабе

--- components/github/github.js
+++ components/github/github.js
@@ -5,6 +5,8 @@
  * @constructor
  */
 var GitHubAPI = function ($, popup) {
+    this._currentToken = '';
+    this._currentUserLogin = '';
 
     /**
      * Возвращает тексты из папки
@@ -84,4 +86,82 @@ var GitHubAPI = function ($, popup) {
     this.getFile = function(fileURL) {
         return $.get(fileURL);
     };
+
+    /**
+     * Возвращает сконвертированный Unicode текст в формате base64, совместимый с github
+     * @param {string} text - исходная Unicode строка
+     * @returns {string}
+     */
+    this.textToBase64 = function (text) {
+        return btoa(unescape(encodeURIComponent(text)));
+    }
+
+    /**
+     * Возвращает jQuery promise который при успешном результате ресолвится данными
+     *
+     * @param {string} branchName - имя будущей ветки
+     * @param {string} parentBranchHash - sha хеш от какой ветки отводить новую ветку
+     * @returns {*|PromiseLike<T>|Promise<T>}
+     */
+    this.createBranch = function (branchName, parentBranchHash) {
+        var branchPath = 'refs/heads/' + this._currentUserLogin + '/' + branchName;
+
+        popup('Создается ветка для сохранения...', 'info');
+        return $
+            .ajax({
+                method: "POST",
+                url: "https://api.github.com/repos/prosvita/QIRIMTATARTILI/git/refs",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'token ' + this._currentToken);
+                },
+                data: JSON.stringify({
+                    ref: branchPath,
+                    sha: parentBranchHash
+                })
+            })
+            .done(function( msg ) {
+                console.log('done triggered', msg)
+            })
+            .fail(function(err) {
+                popup('Не удалось создать ветку ' + branchPath + ' Ошибка:' + err, 'danger');
+            });
+    };
+
+    /**
+     * Этот метод создает коммит с изменениями в одном файле.
+     * Возвращает jQuery promise который при успешном результате ресолвится хешом успешного коммита
+     *
+     * @param {string} content - текст который пушим
+     * @param {string} filePath - путь к файлу в который коммитим
+     * @param {string} branchName - имя будущей ветки
+     * @param {string} parentCommit - sha хеш от какого коммита вносятся изменения в файл
+     * @returns {*|PromiseLike<T>|Promise<T>}
+     */
+    this.pushCommit = function (content, filePath, branchName, parentCommit) {
+        var post = {
+            message: 'test github api',
+            committer: {
+                name: 'Elnur Kurtaliev',
+                email: 'el92@yandex.ru'
+            },
+            branch: branchName,
+            content: this.textToBase64(content),
+            sha: parentCommit
+        };
+
+        return $
+            .ajax({
+                method: 'PUT',
+                url: filePath,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', 'token ' + this._currentToken);
+                },
+                data: JSON.stringify(post)
+            })
+            .done(function (msg) {
+                console.log('done triggered', msg)
+            }).fail(function (err) {
+                popup('Не удалось отправить изменения в файле ' + filePath + ' Ошибка:' + err, 'danger');
+            });
+    }
 };
